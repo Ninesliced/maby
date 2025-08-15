23e089070ce544089c5c9c5e41876b9a195cefae
notgoyome <guillaume1.tran@epitech.eu>
2025-08-15 19:45:49 +0200

    add ui preview cursor

--- scenes/entities/player/components/action_manager.gd
+++ scenes/entities/player/components/action_manager.gd
@@ -14,81 +14,85 @@ signal action_appended(action: Action)
 signal action_set(actions: Array[Action])
 
 func _ready():
-	if !enabled:
-		set_process(false)
-		return
-	var player: Player = get_parent()
-	if not player:
-		assert(false, "ActionManager must be a child of Player.")
-	if !player.player_data:
-		assert(false, "Player has no PlayerData assigned.")
-	player_data = player.player_data
-	_actions = player_data.actions
-	action_set.emit(_actions)
-	SignalBus.tile_clicked.connect(_execute_action)
-
+    if !enabled:
+        set_process(false)
+        return
+    var player: Player = get_parent()
+    if not player:
+        assert(false, "ActionManager must be a child of Player.")
+    if !player.player_data:
+        assert(false, "Player has no PlayerData assigned.")
+    player_data = player.player_data
+    _actions = player_data.actions
+    action_set.emit(_actions)
+    SignalBus.tile_clicked.connect(_execute_action)
+    if _actions.size() > 0:
+        SignalBus.on_new_current_action.emit(_actions[0])
+    
 
 
 # PUBLIC METHODS
 
 func get_front() -> Action:
-	if _actions.size() == 0:
-		return null
-	return _actions[0]
+    if _actions.size() == 0:
+        return null
+    return _actions[0]
 
 ## Returns the first action in the queue
 func pop_next() -> Action:
-	if _actions.size() == 0:
-		return null
-	var action: Action = _try_pop_next(0)
-	if not action:
-		return null
-	action_popped.emit(action)
-	return action
+    if _actions.size() == 0:
+        return null
+    var action: Action = _try_pop_next(0)
+    if not action:
+        return null
+    action_popped.emit(action)
+    return action
 
 ## Appends the action to the end of the queue
 func append(action: Action):
-	if _actions.size() >= max_actions:
-		_try_pop_next(0)
-	_actions.append(action)
-	action_appended.emit(action)
+    if _actions.size() >= max_actions:
+        _try_pop_next(0)
+    _actions.append(action)
+    action_appended.emit(action)
 
 ## Get actions, don't modify the returned array
 func get_actions() -> Array[Action]:
-	return _actions
+    return _actions
 
 
 func _execute_action(tile):
-	if not enabled:
-		return
+    if not enabled:
+        return
 
-	var result = false
-	var action : Action = get_front()
+    var result = false
+    var action : Action = get_front()
 
-	if action:
-		result = action.execute_action(tile)
+    if action:
+        result = action.execute_action(tile)
 
-	if !result:
-		return
+    if !result:
+        return
 
-	_actions.pop_front()
-	action_popped_at.emit(0)
-	SignalBus.on_player_action.emit(GameGlobal.player, action)
-	if !action.temporary:
-		append(action)
+    _actions.pop_front()
+    action_popped_at.emit(0)
+    SignalBus.on_player_action.emit(GameGlobal.player, action)
+    if _actions.size() != 0:
+        SignalBus.on_new_current_action.emit(_actions[0])
+    if !action.temporary:
+        append(action)
 
 func _get_num_of_same_action(action: Action) -> int:
-	var count: int = 0
-	for a in _actions:
-		if a.name == action.name:
-			count += 1
-	return count
+    var count: int = 0
+    for a in _actions:
+        if a.name == action.name:
+            count += 1
+    return count
 
 func _try_pop_next(number: int) -> Action:
-	if _actions.size() - 1 <= number:
-		return null
-	var action_to_pop: Action = _actions[number]
-	if action_to_pop.keep_always_one and _get_num_of_same_action(action_to_pop) <= 1:
-		return _try_pop_next(number + 1)
-	action_popped_at.emit(number)
-	return _actions.pop_at(number)
+    if _actions.size() - 1 <= number:
+        return null
+    var action_to_pop: Action = _actions[number]
+    if action_to_pop.keep_always_one and _get_num_of_same_action(action_to_pop) <= 1:
+        return _try_pop_next(number + 1)
+    action_popped_at.emit(number)
+    return _actions.pop_at(number)
--- scenes/game/mouse_action_ui/action_current.gd
+++ scenes/game/mouse_action_ui/action_current.gd
@@ -1,4 +1,4 @@
-extends Node
+extends Node2D
 class_name ActionCurrentComponent
 
 var action_ui: ActionUI = null
@@ -10,21 +10,12 @@ func _ready() -> void:
 		action_ui = parent_node
 	else:
 		assert(false, "ActionCurrentComponent must be a child of ActionUI node.")
-	GameGlobal.on_action_stack_changed.connect(set_current_action)
-	set_current_action()
+	SignalBus.on_new_current_action.connect(set_current_action)
 
 func _process(_delta: float) -> void:
-	var pos_viewport: Vector2 = get_viewport().get_mouse_position()
-	var cam := get_parent().get_parent().get_node("Camera2D") as Camera2D
-	var world_pos: Vector2 = get_viewport().get_canvas_transform().affine_inverse() * pos_viewport
-	action_ui.global_position = world_pos - Vector2(8, 8)
-	
+	var mouse_position = get_global_mouse_position()
+	action_ui.global_position = mouse_position
 
-func set_current_action():
-	if GameGlobal.action_stacks.size() == 0:
-		action_ui.set_texture_rect(null)
-		return
-	action_ui.animation_pop()
-	var texture = GameGlobal.action_textures[GameGlobal.action_stacks[0]]
-	# print("Current action texture: ",action_ui.texture_rect)
-	action_ui.set_texture_rect(texture)
+func set_current_action(new_action: Action) -> void:
+	var texture = new_action.texture
+	action_ui.set_texture(texture)
