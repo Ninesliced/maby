3c5341b39ffd403ae8ba26cb95e8553409c67a0d
Guillame Tran <guillaume1.tran@epitech.eu>
2025-08-16 18:44:20 +0200

    disable pause manager

--- addons/shaker/components/ui/pause_manager/pause_manager.gd
+++ addons/shaker/components/ui/pause_manager/pause_manager.gd
@@ -8,22 +8,4 @@ var paused := false
 
 func _ready() -> void:
 	assert(pause_menu, "No menu defined")
-	pause_menu_node = load(pause_menu).instantiate()
-
-func _input(event: InputEvent) -> void:
-	if event.is_action_pressed(pause_action):
-		_toggle_pause()
-
-func _pause():
-	paused = true
-	UIManager.set_ui(pause_menu_node)
-
-func _unpause():
-	paused = false
-	UIManager.close_ui()
-
-func _toggle_pause():
-	if paused:
-		_unpause()
-	else:
-		_pause()
+	# pause_menu_node = load(pause_menu).instantiate()
